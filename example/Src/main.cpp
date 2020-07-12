2073a2d063b43e359925dd52f3fe9b9b0e35c020
xiaoHu <hu2240877635@gmail.com>
2020-07-12 16:52:27 +0800

    cubeMX example

---
+++ example/Src/main.cpp
@@ -0,0 +1,446 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "i2c.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+//#include "MPU6050_6Axis_MotionApps20.h"
+#include "MPU6050_6Axis_MotionApps_V6_12.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+/* =========================================================================
+   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
+   depends on the MPU-6050's INT pin being connected to the Arduino's
+   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
+   digital I/O pin 2.
+   ========================================================================= */
+
+/* =========================================================================
+   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
+   when using Serial.write(buf, len). The Teapot output uses this method.
+   The solution requires a modification to the Arduino USBAPI.h file, which
+   is fortunately simple, but annoying. This will be fixed in the next IDE
+   release. For more info, see these links:
+
+   http://arduino.cc/forum/index.php/topic,109987.0.html
+   http://code.google.com/p/arduino/issues/detail?id=958
+   ========================================================================= */
+
+
+
+// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+// quaternion components in a [w, x, y, z] format (not best for parsing
+// on a remote host such as Processing or something though)
+//#define OUTPUT_READABLE_QUATERNION
+
+// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+// (in degrees) calculated from the quaternions coming from the FIFO.
+// Note that Euler angles suffer from gimbal lock (for more info, see
+// http://en.wikipedia.org/wiki/Gimbal_lock)
+//#define OUTPUT_READABLE_EULER
+
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
+// components with gravity removed. This acceleration reference frame is
+// not compensated for orientation, so +X is always +X according to the
+// sensor, just without the effects of gravity. If you want acceleration
+// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
+//#define OUTPUT_READABLE_REALACCEL
+
+// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
+// components with gravity removed and adjusted for the world frame of
+// reference (yaw is relative to initial orientation, since no magnetometer
+// is present in this case). Could be quite handy in some cases.
+//#define OUTPUT_READABLE_WORLDACCEL
+
+// uncomment "OUTPUT_TEAPOT" if you want output that matches the
+// format used for the InvenSense teapot demo
+//#define OUTPUT_TEAPOT
+
+
+
+
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 gy;         // [x, y, z]            gyro sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+
+int16_t a_x,a_y,a_z,g_x,g_y,g_z;//6 motion
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    HAL_Delay(500);
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_USART1_UART_Init();
+    /* USER CODE BEGIN 2 */
+    // initialize device
+
+    printf(F("Initializing I2C devices...\n"));//F(x)=x
+    mpu.initialize();
+
+
+    // verify connection
+    printf(F("Testing device connections...\n"));
+//    while(!mpu.testConnection())
+//    {
+//        HAL_I2C_MspDeInit(&hi2c1);
+//        __HAL_RCC_GPIOB_CLK_DISABLE();
+//        HAL_Delay(250);
+//        HAL_I2C_MspInit(&hi2c1);
+//        HAL_Delay(250);
+//        HAL_UART_Transmit(&huart1,(uint8_t *)".",1,100);
+//        //HAL_NVIC_SystemReset();
+//    }
+    printf(mpu.testConnection() ? F("MPU6050 connection successful\n") : F("MPU6050 connection failed\n"));
+
+    // wait for ready
+
+
+    // load and configure the DMP
+    printf(F("Initializing DMP...\n"));
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(30);
+    mpu.setYGyroOffset(58);
+    mpu.setZGyroOffset(21);
+    mpu.setXAccelOffset(0);
+    mpu.setYAccelOffset(0);
+    mpu.setZAccelOffset(0);
+
+    devStatus = mpu.dmpInitialize();
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0)
+    {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        printf(F("Enabling DMP...\n"));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        printf(F("DMP ready! Waiting for first interrupt...\n"));
+//        float a;
+//        printf("int is %d\n",sizeof(a));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+    else
+    {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        printf(F("DMP Initialization failed (code "));
+        printf("%d",devStatus);
+        printf(F(")\n"));
+    }
+
+
+/* USER CODE END 2 */
+
+/* Infinite loop */
+/* USER CODE BEGIN WHILE */
+while (1)
+{
+/* USER CODE END WHILE */
+    if (!dmpReady)
+        continue;
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
+    {
+/* USER CODE BEGIN 3 */
+#ifdef OUTPUT_READABLE_QUATERNION
+        // display quaternion values in easy matrix form: w x y z
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        printf("quat\t");
+        printf(q.w);
+        printf("\t");
+        printf(q.x);
+        printf("\t");
+        printf(q.y);
+        printf("\t");
+        printfln(q.z);
+#endif
+
+#ifdef OUTPUT_READABLE_EULER
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetEuler(euler, &q);
+        printf("euler\t");
+        printf(euler[0] * 180 / M_PI);
+        printf("\t");
+        printf(euler[1] * 180 / M_PI);
+        printf("\t");
+        printfln(euler[2] * 180 / M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        //mpu.getMotion6(&a_x, &a_y, &a_z, &g_x, &g_y, &g_z);
+        //printf("6:\t%d\t%d\t%d\t%d\t%d\t%d\n", a_x, a_y, a_z, g_x, g_y, g_z);
+        printf("ypr:\t");
+        printf("%f", ypr[0] * 180 / M_PI);
+        printf("\t");
+        printf("%f", ypr[1] * 180 / M_PI);
+        printf("\t");
+        printf("%f", ypr[2] * 180 / M_PI);
+        /*
+          mpu.dmpGetAccel(&aa, fifoBuffer);
+          printf("\tRaw Accl XYZ\t");
+          printf(aa.x);
+          printf("\t");
+          printf(aa.y);
+          printf("\t");
+          printf(aa.z);
+          mpu.dmpGetGyro(&gy, fifoBuffer);
+          printf("\tRaw Gyro XYZ\t");
+          printf(gy.x);
+          printf("\t");
+          printf(gy.y);
+          printf("\t");
+          printf(gy.z);
+        */
+        printf("\n");
+
+#endif
+
+#ifdef OUTPUT_READABLE_REALACCEL
+        // display real acceleration, adjusted to remove gravity
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        printf("areal\t");
+        printf(aaReal.x);
+        printf("\t");
+        printf(aaReal.y);
+        printf("\t");
+        printfln(aaReal.z);
+#endif
+
+#ifdef OUTPUT_READABLE_WORLDACCEL
+        // display initial world-frame acceleration, adjusted to remove gravity
+        // and rotated based on known orientation from quaternion
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+        printf("aworld\t");
+        printf(aaWorld.x);
+        printf("\t");
+        printf(aaWorld.y);
+        printf("\t");
+        printfln(aaWorld.z);
+#endif
+
+#ifdef OUTPUT_TEAPOT
+        // display quaternion values in InvenSense Teapot demo format:
+        teapotPacket[2] = fifoBuffer[0];
+        teapotPacket[3] = fifoBuffer[1];
+        teapotPacket[4] = fifoBuffer[4];
+        teapotPacket[5] = fifoBuffer[5];
+        teapotPacket[6] = fifoBuffer[8];
+        teapotPacket[7] = fifoBuffer[9];
+        teapotPacket[8] = fifoBuffer[12];
+        teapotPacket[9] = fifoBuffer[13];
+        Serial.write(teapotPacket, 14);
+        teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+#endif
+        printf("good\n");
+        HAL_Delay(10);
+    }
+    else{
+        printf("fifo error!\n");
+        HAL_Delay(1000);
+    }
+}
+/* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ example/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/DependInfo.cmake
@@ -0,0 +1,95 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/startup/startup_stm32f103xb.s" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/gpio.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/gpio.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/i2c.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/i2c.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/stm32f1xx_hal_msp.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/stm32f1xx_it.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/stm32f1xx_it.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/syscalls.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/syscalls.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/system_stm32f1xx.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/system_stm32f1xx.c.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/usart.c" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/usart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/ArduinoWrapper.cpp" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/ArduinoWrapper.cpp.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/I2Cdev.cpp" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/I2Cdev.cpp.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/MPU6050.cpp" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/MPU6050.cpp.obj"
+  "/home/hu/CLionProjects/HAL_MPU6050_DMP/Src/main.cpp" "/home/hu/CLionProjects/HAL_MPU6050_DMP/cmake-build-debug/CMakeFiles/HAL_MPU6050_DMP.elf.dir/Src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
